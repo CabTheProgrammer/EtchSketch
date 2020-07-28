2d1165e5aa49f4ac754184e50fbc8652d111a648
CabTheDeveloper <cbartley876@gmail.com>
2020-07-28 00:00:34 -0500

    Made good progress and learnt some stuff. Hope I don't forget!

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ const container = document.querySelector('#Sandbox');
     let pixel_div =[] 
 
 function divcreate(divSize)
-{
+{   let string = "auto ";
+    
+    
+    for(var diff = 1;diff < (+divSize);diff++)
+    {
+        string += "auto ";
+    }
     //for loop to generate divs
     for(var count = 0;count < (+divSize)**2;count++)
         {
+            
             let n_div = document.createElement('div')
             n_div.className = "blank";
             n_div.textContent = "You Can Make IT!";
             container.appendChild(n_div);
 
         }
-
+        container.style.setProperty('--columnNum',string)
+        console.log(string);       
        
 }
 
-divcreate(20);
+divcreate(32);
