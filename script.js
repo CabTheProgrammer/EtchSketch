86a4e2228fd961eec7b539e55f5b6e0a50b2f839
CabTheDeveloper <cbartley876@gmail.com>
2020-07-29 01:29:35 -0500

    Finished? it looks so basic! but the requirements are met

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-const container = document.querySelector('#Sandbox');
-    // This is for the 'pixel' divs? Idk, I'm keeping it
-    let pixel_div =[] 
+// This is for the 'pixel' divs? Idk, I'm keeping it as pixels
+let pixelSize = 1;
 
-function divcreate(divSize)
-{   let string = "auto ";
+const container = document.querySelector('#Sandbox');
+const resize = document.querySelector('#resize');
+const clear = document.querySelector('#clear');
+
+// going to use this function to erase the old grid
+function reset() {    
+    container.innerHTML = "";//Yup this works!
+    console.log("Reset Called");
+}
+
+
+function divcreate(divSize) {
+    let string = "auto ";
+    if(container.childElementCount != 0)
+    reset();
     
-    
-    for(var diff = 1;diff < (+divSize);diff++)
-    {
+    for (var diff = 1; diff < (+divSize); diff++) {
         string += "auto ";
     }
     //for loop to generate divs
-    for(var count = 0;count < (+divSize)**2;count++)
-        {
-            
-            let n_div = document.createElement('div')
-            n_div.className = "blank";
-            n_div.textContent = "You Can Make IT!";
-            container.appendChild(n_div);
+    for (var count = 0; count < (+divSize) ** 2; count++) {
 
-        }
-        container.style.setProperty('--columnNum',string)
-        console.log(string);       
-       
+        let n_div = document.createElement('div')
+        n_div.className = "blank";
+        n_div.textContent = "You Can Make IT!";
+        n_div.addEventListener('mouseenter',() =>n_div.className = "hilight")
+        container.appendChild(n_div);
+
+    }
+    container.style.setProperty('--columnNum', string)
+    console.log("DivCreate Called!");
+    
+
 }
 
-divcreate(32);
+
+clear.addEventListener('click', () => { divcreate(pixelSize) })
+resize.addEventListener('click', () => { pixelSize = prompt("What's the size of the grid?"); divcreate(pixelSize); })
+
+
+
+divcreate(pixelSize);
